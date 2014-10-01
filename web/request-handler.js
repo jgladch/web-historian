6e29712e447716bae69246ae6e4739c6620f5adc
jgladch <jeffgladchun@gmail.com>
2014-10-01 16:24:37 -0700

    Serving index.html, first test passing

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,26 @@
 var path = require('path');
+var url = require('url');
 var archive = require('../helpers/archive-helpers');
+var helpers = require("./http-helpers");
+var fs = require('fs');
 // require more modules/folders here!
 
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+
+  console.log("Serving request type " + req.method + " for url " + req.url);
+
+  var statusCode = 404;
+
+  if (req.method === 'GET') {
+    fs.readFile(archive.paths.siteAssets+'/index.html', function(err, file){
+      if (err) {
+        console.log("ERROR! File is: "+file);
+      }
+      statusCode = 200;
+      res.writeHead(statusCode, helpers.headers);
+      res.write(file, 'utf8');
+      res.end();
+    });
+  }
 };
